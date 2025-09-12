746c2411b2e1edc44f200beb03dd2c7c2fe23a29
swedenebomi <swedenebomi@gamil.com>
2025-09-12 22:19:18 +0200

    feat(router) : ingegrate Navbar into App with routing

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Home from './pages/Home';
 import Countries from './pages/Countries';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <>
       <Router>
+        <Navbar />
         <Routes>
           <Route path='/'  element={<Home />}/>
           <Route path='/countries'  element={<Countries />}/>
