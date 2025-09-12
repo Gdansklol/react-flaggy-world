37b67328a8fbb8fc71ac6fdbcf06e5e1b940d483
swedenebomi <swedenebomi@gamil.com>
2025-09-12 22:03:17 +0200

    feat(router) : set up basic routing with Countries page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
 import './App.css';
+import Home from './pages/Home';
+import Countries from './pages/Countries';
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Router>
         <Routes>
           <Route path='/'  element={<Home />}/>
+          <Route path='/countries'  element={<Countries />}/>
         </Routes>
       </Router>
     </>
