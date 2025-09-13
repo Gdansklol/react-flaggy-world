d513676b32498d20a242ebd2b9d49869d28ac84d
swedenebomi <swedenebomi@gamil.com>
2025-09-13 02:59:18 +0200

    feat(countries): add region buttons, display flags with names, and enable navigation to detail page

--- src/pages/Countries.jsx
+++ src/pages/Countries.jsx
@@ -1,31 +1,44 @@
 import { useDispatch, useSelector } from "react-redux";
 import { fetchCountriesRegion } from "../redux/countriesSlice";
+import { useNavigate } from "react-router-dom";
+import '../css/countries.css';
 
 const regions = ["Europe", "Asia", "Oceania", "Americas", "Africa"];
 
 const Countries = () => {
   const dispatch = useDispatch();
+  const navigate = useNavigate();
   const {list, status, error} = useSelector((state)=> state.countries);
 
   return (
     <div>
       <h2>Countries Page</h2>
+      <div className="">
       {regions.map((region)=> (
         <button key={region} onClick={()=> dispatch(fetchCountriesRegion(region.toLowerCase()))} >
           {region}
         </button>
       ))}
+      </div>
       
-
       {status === "loading" && <p>Loading...</p>}
       {status === "failed" && <p>Error: {error}</p>}
 
       {status === "success" && (
-        <ul>
+        <div>
           {list.map((country) => (
-            <li key={country.cca3}>{country.name.common}</li>
+            <div 
+            key={country.cca3}
+            onClick={()=>navigate(`/countries/${country.name.common}`)}
+            >
+              <img 
+              src={country.flags.png}
+              ald={country.name.common}
+              />
+              <p>{country.name.common}</p>
+            </div>
           ))}
-        </ul>
+        </div>
       )}
     </div>
   )
