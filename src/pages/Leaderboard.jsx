9c0cecdbedf3f92fef1b4379cb3814219d77603e
swedenebomi <swedenebomi@gamil.com>
2025-09-16 13:20:34 +0200

    Leaderboard: render quiz usersResults list with map

--- src/pages/Leaderboard.jsx
+++ src/pages/Leaderboard.jsx
@@ -1,15 +1,32 @@
-import {useState} from 'react'
+import {useState,useEffect} from 'react'
 
 const Leaderboard = () => {
   const [results, setResults] = useState([]);
 
+  useEffect(()=> {
+    const savedInStorage = JSON.parse(localStorage.getItem("quizResults")) || [];
+    setResults(savedInStorage);
+  },[])
+
   return (
     <div>
     <h2> Leaderboard</h2>
-
-    <p>Results:</p>
+     <h3>Users Results</h3>
+    {results.lenght === 0 ? (
+      <p>No results yet!</p>
+    ) : (
+      <ul>
+        {results.map((userResult,index)=> (
+          <li key={index}>{userResult.username} - 
+          {userResult.score}  points in {""}
+          ({userResult.region})
+          </li>
+        ) )}
+      </ul>
+    )}
+   
     </div>
   )
 }
 
-export default Leaderboard
+export default Leaderboard;
