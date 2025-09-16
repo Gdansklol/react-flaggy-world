58fc5f2e574e45c7fe0fac4d1e57b3a447520871
swedenebomi <swedenebomi@gamil.com>
2025-09-16 23:14:46 +0200

    Leaderboard : Updated Object.entries map syntax for proper destructuring

--- src/pages/Leaderboard.jsx
+++ src/pages/Leaderboard.jsx
@@ -6,27 +6,46 @@ const Leaderboard = () => {
   useEffect(()=> {
     const savedInStorage = JSON.parse(localStorage.getItem("quizResults")) || [];
     setResults(savedInStorage);
-  },[])
+  },[]);
 
+  const regionsGroup = {};
+  results.forEach((userResult)=> {
+    const regionOfStorage = userResult.region;
+    if(!regionsGroup[regionOfStorage]) {
+      regionsGroup[regionOfStorage] = [];
+    }
+      regionsGroup[regionOfStorage].push(userResult)
+
+      console.log(regionsGroup);
+  })
+ 
   return (
     <div>
     <h2> Leaderboard</h2>
      <h3>Users Results</h3>
-    {results.lenght === 0 ? (
+
+    {results.length === 0 ? (
       <p>No results yet!</p>
     ) : (
+      <>
+      {Object.entries(regionsGroup).map(([region,users]) => (
+        <section key={region}>
+          <h3>{region}</h3>
       <ul>
-        {results.map((userResult,index)=> (
-          <li key={index}>{userResult.username} - 
+        {users.map((userResult,index)=> (
+          <li key={index}>
+            {userResult.username} - 
           {userResult.score}  points in {""}
           ({userResult.region})
           </li>
-        ) )}
+        ))}
       </ul>
-    )}
-   
+       </section>
+    ))}
+   </>
+  )}
     </div>
-  )
-}
+  );
+};
 
 export default Leaderboard;
